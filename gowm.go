b8bd955f9249f75d806cb178e28007c3ed2f58c1
Brandon Powell <bpowell29a@gmail.com>
2014-10-05 22:00:25 -0400

    Base event loop

--- gowm.go
+++ gowm.go
@@ -4,14 +4,28 @@ import (
 	"fmt"
 
 	"github.com/BurntSushi/xgb"
+	"github.com/BurntSushi/xgb/xproto"
 )
 
 func main() {
-	X, err := xgb.NewConn()
+	X, err := xgb.NewConnDisplay(":0")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-    defer X.Close()
+	defer X.Close()
+
+	setup := xproto.Setup(X)
+	screen := setup.DefaultScreen(X)
+	xproto.ChangeWindowAttributes(X, screen.Root, xproto.CwBackPixel|xproto.CwEventMask, []uint32{0xffffffff, xproto.EventMaskStructureNotify | xproto.EventMaskKeyPress | xproto.EventMaskKeyRelease})
+
+	for {
+		ev, err := X.WaitForEvent()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("Event: %s", ev)
+	}
 
 }
